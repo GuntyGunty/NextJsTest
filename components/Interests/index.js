e12a0d8c5e739c9906abf27188a12bde51fce2e1
Деловая куница <gunter0705@gmail.com>
2018-10-25 17:20:29 +0300

    Исправляет ссылки в интересах

--- components/Interests/index.js
+++ components/Interests/index.js
@@ -14,24 +14,40 @@ class Interests extends Component {
         <h2 className="interests__title">Интересы</h2>
         <ul className="interests__list">
           <li className="interests__item">
-            <img className="interests__img" src="/static/icon1.png" width="60" height="60" />
-            <span className="interests__item-title">eiusmod</span>
-            <p className="interests__item-descr">laboris commodo nostrud</p>
+            <Link href="/">
+              <a className="interests__item-link">
+                <img className="interests__img" src="/static/icon1.png" width="60" height="60" />
+                <span className="interests__item-title">eiusmod</span>
+                <p className="interests__item-descr">laboris commodo nostrud</p>
+              </a>
+            </Link>
           </li>
           <li className="interests__item">
-            <img className="interests__img" src="/static/icon2.png" width="60" height="60" />
-            <span className="interests__item-title">voluptate</span>
-            <p className="interests__item-descr">voluptate cupidatat sint officia esse ea proident</p>
+            <Link href="/">
+              <a className="interests__item-link">
+                <img className="interests__img" src="/static/icon2.png" width="60" height="60" />
+                <span className="interests__item-title">voluptate</span>
+                <p className="interests__item-descr">voluptate cupidatat sint officia esse ea proident</p>
+              </a>
+            </Link>
           </li>
           <li className="interests__item">
-            <img className="interests__img" src="/static/icon3.png" width="60" height="60" />
-            <span className="interests__item-title">Velit</span>
-            <p className="interests__item-descr">ullamco eiusmod voluptate cupidatat sint</p>
+            <Link href="/">
+              <a className="interests__item-link">
+                <img className="interests__img" src="/static/icon3.png" width="60" height="60" />
+                <span className="interests__item-title">Velit</span>
+                <p className="interests__item-descr">ullamco eiusmod voluptate cupidatat sint</p>
+              </a>
+            </Link>
           </li>
           <li className="interests__item">
-            <img className="interests__img" src="/static/icon4.png" width="60" height="60" />
-            <span className="interests__item-title">ullamco</span>
-            <p className="interests__item-descr">llabore sit magna duis ex id veniam</p>
+            <Link href="/">
+              <a className="interests__item-link">
+                <img className="interests__img" src="/static/icon4.png" width="60" height="60" />
+                <span className="interests__item-title">ullamco</span>
+                <p className="interests__item-descr">llabore sit magna duis ex id veniam</p>
+              </a>
+            </Link>
           </li>
         </ul>
       </section>
